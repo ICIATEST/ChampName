f0817e74f1cdd22bef245282697c9bd9b3869dcb
ICIATEST <109563388+ICIATEST@users.noreply.github.com>
2022-07-21 10:22:44 +0900

    ChampionName - 아리를 추가함

--- pythonProject4/main.py
+++ pythonProject4/main.py
@@ -2,6 +2,10 @@ import requests
 
 
 url = 'http://ddragon.leagueoflegends.com/cdn/12.13.1/data/ko_KR/champion/Aatrox.json'
+url2 = 'http://ddragon.leagueoflegends.com/cdn/12.13.1/data/ko_KR/champion/Ahri.json'
+
 res = requests.get(url).json()
+res2 = requests.get(url2).json()
 
 champ_lst = (res['data']['Aatrox']['key'], res['data']['Aatrox']['name'])
+champ_lst2 = (res2['data']['Aatrox']['key'], res2['data']['Aatrox']['name'])
